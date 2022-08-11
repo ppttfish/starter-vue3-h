3aadb6ce4b508048609b81f8850f3a38f952e858
sunchenyu <sunchenyu96@163.com>
2022-08-11 14:30:48 +0800

    chore: 新增auto-import；index.html禁止缩放

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      reactivityTransform: true
+    }),
+    AutoImport({
+      imports: ['vue', 'vue/macros', 'vue-router'],
+      dts: true
+    }),
     Components({
       resolvers: [VantResolver()]
     })
