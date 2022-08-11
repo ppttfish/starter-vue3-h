548cc1208089bac21303be53b3cf34a5077207fa
sunchenyu <sunchenyu96@163.com>
2022-08-11 11:59:58 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <van-grid column-num="2">
+      <van-grid-item>
+        <RouterLink to="/">首页</RouterLink>
+      </van-grid-item>
+      <van-grid-item>
+        <RouterLink to="/about">About</RouterLink>
+      </van-grid-item>
+    </van-grid>
+  </div>
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="about">
+    <van-button>好的</van-button>
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+h1 {
+  font-size: 32px;
+}
+</style>
